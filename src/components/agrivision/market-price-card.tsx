cef9248a9a23c4d3bfd919487a7b59c6824d0241
SAGAR <officialsagarbunker@gmail.com>
2025-09-15 11:32:18 +0000

    still it is not corrected

--- src/components/agrivision/market-price-card.tsx
+++ src/components/agrivision/market-price-card.tsx
@@ -20,8 +20,6 @@ const TrendIcon = ({ trend }: { trend: 'up' | 'down' | 'stable' }) => {
 const RupeeIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -30,10 +28,11 @@ const RupeeIcon = (props: React.SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M6 3h12" />
-    <path d="M6 8h12" />
-    <path d="m6 13 8.5 8" />
-    <path d="M7 13h6" />
+    <path d="M15.5 8.5H18" />
+    <path d="M15.5 13.5H18" />
+    <path d="M6 3L10 13" />
+    <path d="M6 21L10 13" />
+    <path d="M10 13H18" />
   </svg>
 );
 
